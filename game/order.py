8b6b9487de10b69f8c50123a7f21a0c8d0650650
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-11-23 13:38:33 +0100

    Fin du jeu

--- game/order.py
+++ game/order.py
@@ -2,40 +2,191 @@ from itertools import groupby
 import sys
 
 
-def get_quinte_flush(big_hand):
-	grouped = [list(group) for key, group in groupby(sorted([card.color for card in big_hand]))]
+def getweight(hand):
+	return sum([card.weight for card in hand])
 
-# ====================
-# 	print([c.weight for c in big_hand])
-	# print(max(big_hand, key=lambda x: x.weight))
-# ====================
 
-	flush = [group for group in grouped if len(group) >= 5]
-	if not flush:
-		return None
-	print(flush)
+def combination(bighand, hand, length):
+	grouped = [list(group) for key, group in groupby(sorted(card.weight for card in bighand))]
+	square = [g for g in grouped if len(g) == length]
+	if not square:
+		return False
+	for card in bighand:
+		if card.weight == square[0][0]:
+			hand.append(card)
+	return True
 
-	# if 5 in [len(list(group)) for key, group in groupby(sorted([card.color for card in big_hand]))]:
-	# 	temp = filter(lambda x: )
+
+def isquinteflushroyal(bighand, hand):
+	if isquinteflush(bighand, hand):
+		return hand[0].value == 'As'
+	return False
+
+
+def isquinteflush(bighand, hand):
+	tmphand = []
+	if iscolor(bighand, tmphand) and isquinte(tmphand, hand):
+		return True
+	return False
+
+
+def issquare(bighand, hand):
+	return combination(bighand, hand, 4)
+
+
+def isfull(bighand, hand):
+	brelan = []
+	if not isbrelan(bighand, brelan):
+		return False
+	rest = [card for card in bighand if card not in brelan]
+	pair = []
+	if not ispair(rest, pair):
+		return False
+	rest = [card for card in rest if card not in pair]
+	otherpair = []
+	if ispair(rest, otherpair):
+		pair = pair if getweight(pair) > getweight(otherpair) else otherpair
+
+	hand.extend(brelan + pair)
+	return True
+
+
+def iscolor(bighand, hand):
+	grouped = [list(group) for key, group in groupby(sorted(card.color for card in bighand))]
+	color = [g for g in grouped if len(g) >= 5]
+	if not color:
+		return False
+	for card in bighand:
+		if card.color == color[0][0]:
+			hand.append(card)
+	return True
+
+
+def isquinte(bighand, hand):
+	sort = sorted([card.weight for card in bighand], reverse=True)
+	quinte = []
+	previous = -1
+	for value in sort:
+		if previous != -1:
+			if value == previous - 1:
+				quinte.append(value)
+				if len(quinte) == 5:
+					break
+			else:
+				quinte = []
+		else:
+			quinte.append(value)
+		previous = value
+	if len(quinte) < 5:
+		return False
+	for weight in quinte:
+		for card in bighand:
+			if weight == card.weight:
+				hand.append(card)
+				break
+	return True
+
+
+def isbrelan(bighand, hand):
+	return combination(bighand, hand, 3)
+
+
+def isdoublepair(bighand, hand):
+	first = []
+	if not ispair(bighand, first):
+		return False
+	rest = [card for card in bighand if card not in first]
+	second = []
+	if not ispair(rest, second):
+		return False
+	if getweight(first) > getweight(second):
+		hand.extend(first + second)
+	else:
+		hand.extend(second + first)
+	return True
+
+
+def ispair(bighand, hand):
+	return combination(bighand, hand, 2)
+
+
+def completehand(bighand, hand):
+	rest = [card for card in bighand if card not in hand]
+	while len(hand) < 5:
+		maxcard = None
+		for card in rest:
+			if maxcard is None:
+				maxcard = card
+			elif card.weight > maxcard.weight:
+				maxcard = card
+		rest.remove(maxcard)
+		hand.append(maxcard)
+
+
+def getwinner(players):
+	handstate = max(p[1] for p in players)
+	potential = [p[0] for p in players if p[1] == handstate]
+	for i in range(5):
+		maxplayer = None
+		equal = False
+		for p in potential:
+			if maxplayer is None:
+				maxplayer = p
+			elif maxplayer[i].weight < p[i].weight:
+				maxplayer = p
+			elif maxplayer[i].weight == p[i].weight:
+				equal = True
+		if equal:
+			continue
+		else:
+			return maxplayer
 	return None
 
 
-def get_best_hand(river, hands):
-	weights = []
-	for hand in hands:
-		big_hand = river.cards + hand.cards
+def getbesthand(river, hands):
+	players = []
+	for h in hands:
+		bighand = river.cards + h.cards
+		hand = []
 
-		sum = 0
-		for card in big_hand:
-			sum += card.weight
-		weights.append(sum)
+		if isquinteflushroyal(bighand, hand):
+			value = 9
+			print('Quinte Flush Royal')
+		elif isquinteflush(bighand, hand):
+			value = 8
+			print('Quinte Flush')
+		elif issquare(bighand, hand):
+			value = 7
+			print('Square')
+		elif isfull(bighand, hand):
+			value = 6
+			print('Full')
+		elif iscolor(bighand, hand):
+			value = 5
+			print('Color')
+		elif isquinte(bighand, hand):
+			value = 4
+			print('Quinte')
+		elif isbrelan(bighand, hand):
+			value = 3
+			print('Brelan')
+		elif isdoublepair(bighand, hand):
+			value = 2
+			print('Double pair')
+		elif ispair(bighand, hand):
+			value = 1
+			print('Pair')
+		else:
+			value = 0
+			print('Highest card')
 
-		# temp = get_quinte_flush(big_hand)
+		if len(hand) < 5:
+			completehand(bighand, hand)
 
-		# for h in big_hand:
-		# 	print(h)
-	print(weights)
-	return None
+		players.append((hand, value))
+		print([(card.color, card.value) for card in hand])
+
+	return getwinner(players)
 
 
 def main():
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 
+from card.Card import Card
 from card.Deck import Deck
 from game.Hand import Hand
-from game.order import get_best_hand
+from game.order import getbesthand
 
 
-def distribute(deck, nb_of_players):
-	players = [Hand("player"+str(i+1)) for i in range(nb_of_players)]
+def distribute(deck, numberofplayers):
+	players = [Hand("player"+str(i+1)) for i in range(numberofplayers)]
 	for a in range(2):
 		deck.throw_card()
-		for b in range(nb_of_players):
+		for b in range(numberofplayers):
 			players[b].add_card(deck.pick_card())
 
 	return players
@@ -35,8 +36,37 @@ def main():
 	players = distribute(deck, 2)
 	river = get_river(deck)
 
-	winner = get_best_hand(river, players)
+	winner = getbesthand(river, players)
+	if winner is None:
+		print("Equality !")
+	else:
+		print("The winner hand")
+		print([(card.color, card.value) for card in winner])
+
+
+def test():
+	player_1 = Hand("Player1")
+	player_1.add_card(Card('Heart', 'As', 14))
+	player_1.add_card(Card('Heart', 'King', 13))
+	player_2 = Hand("Player2")
+	player_2.add_card(Card('Diamond', '5', 5))
+	player_2.add_card(Card('Diamond', '6', 6))
+	players = [player_1, player_2]
+	river = Hand("River")
+	river.add_card(Card('Heart', 'Queen', 12))
+	river.add_card(Card('Heart', 'Jack', 11))
+	river.add_card(Card('Heart', '10', 10))
+	river.add_card(Card('Heart', '9', 9))
+	river.add_card(Card('Heart', '8', 8))
+
+	winner = getbesthand(river, players)
+	if winner is None:
+		print("Equality !")
+	else:
+		print("The winner hand")
+		print([(card.color, card.value) for card in winner])
 
 
 if __name__ == "__main__":
-	main()
+	# main()
+	test()
